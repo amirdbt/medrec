335ae267e1aa5c8ac6789aef0f997400254a7ad2
amirdbt <ahmed.dambatta@outsourceglobal.com>
2020-06-09 15:01:20 +0100

    added cards for records component

--- src/components/Profile/Records.jsx
+++ src/components/Profile/Records.jsx
@@ -1,12 +1,218 @@
-import React from 'react'
-import {Typography,Grid} from "@material-ui/core"
+import React from "react";
+import {
+  Typography,
+  Grid,
+  Card,
+  CardContent,
+  CardActionArea,
+  CardActions,
+  CardMedia,
+  Button,
+  makeStyles,
+} from "@material-ui/core";
+import { GetApp, Visibility } from "@material-ui/icons";
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+    width: "30%",
+    marginRight: "20px",
+    marginTop: "20px",
+  },
+  media: {
+    height: 170,
+  },
+  cards: {
+    display: "flex",
+    flexWrap: "wrap",
+  },
+});
 
 const Records = () => {
-    return (
-       <Grid container>
-           <Typography variant="h6">Records...</Typography>
-       </Grid>
-    )
-}
+  const classes = useStyles();
+  return (
+    <div className={classes.cards}>
+      <Card className={classes.root}>
+        <CardActionArea>
+          <CardMedia
+            className={classes.media}
+            image="https://img.pngio.com/file-png-image-royalty-free-stock-png-images-for-your-design-file-png-256_256.png"
+            title="Contemplative Reptile"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              Example.png
+            </Typography>
+            <Typography
+              variant="body2"
+              color="textSecondary"
+              component="p"
+            ></Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button size="small" color="primary">
+            <GetApp style={{ marginRight: "5px" }} />
+            Download
+          </Button>
+          <Button size="small" color="primary">
+            <Visibility style={{ marginRight: "5px" }} />
+            Preview
+          </Button>
+        </CardActions>
+      </Card>
 
-export default Records
+      <Card className={classes.root}>
+        <CardActionArea>
+          <CardMedia
+            className={classes.media}
+            image="https://img.pngio.com/file-png-image-royalty-free-stock-png-images-for-your-design-file-png-256_256.png"
+            title="Contemplative Reptile"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              Example2.png
+            </Typography>
+            <Typography
+              variant="body2"
+              color="textSecondary"
+              component="p"
+            ></Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button size="small" color="primary">
+            <GetApp style={{ marginRight: "5px" }} />
+            Download
+          </Button>
+          <Button size="small" color="primary">
+            <Visibility style={{ marginRight: "5px" }} />
+            Preview
+          </Button>
+        </CardActions>
+      </Card>
+
+      <Card className={classes.root}>
+        <CardActionArea>
+          <CardMedia
+            className={classes.media}
+            image="https://img.pngio.com/file-png-image-royalty-free-stock-png-images-for-your-design-file-png-256_256.png"
+            title="Contemplative Reptile"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              Example3.png
+            </Typography>
+            <Typography
+              variant="body2"
+              color="textSecondary"
+              component="p"
+            ></Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button size="small" color="primary">
+            <GetApp style={{ marginRight: "5px" }} />
+            Download
+          </Button>
+          <Button size="small" color="primary">
+            <Visibility style={{ marginRight: "5px" }} />
+            Preview
+          </Button>
+        </CardActions>
+      </Card>
+
+      <Card className={classes.root}>
+        <CardActionArea>
+          <CardMedia
+            className={classes.media}
+            image="https://img.pngio.com/file-png-image-royalty-free-stock-png-images-for-your-design-file-png-256_256.png"
+            title="Contemplative Reptile"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              Example4.png
+            </Typography>
+            <Typography
+              variant="body2"
+              color="textSecondary"
+              component="p"
+            ></Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button size="small" color="primary">
+            <GetApp style={{ marginRight: "5px" }} />
+            Download
+          </Button>
+          <Button size="small" color="primary">
+            <Visibility style={{ marginRight: "5px" }} />
+            Preview
+          </Button>
+        </CardActions>
+      </Card>
+
+      <Card className={classes.root}>
+        <CardActionArea>
+          <CardMedia
+            className={classes.media}
+            image="https://img.pngio.com/file-png-image-royalty-free-stock-png-images-for-your-design-file-png-256_256.png"
+            title="Contemplative Reptile"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              Example5.png
+            </Typography>
+            <Typography
+              variant="body2"
+              color="textSecondary"
+              component="p"
+            ></Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button size="small" color="primary">
+            <GetApp style={{ marginRight: "5px" }} />
+            Download
+          </Button>
+          <Button size="small" color="primary">
+            <Visibility style={{ marginRight: "5px" }} />
+            Preview
+          </Button>
+        </CardActions>
+      </Card>
+
+      <Card className={classes.root}>
+        <CardActionArea>
+          <CardMedia
+            className={classes.media}
+            image="https://img.pngio.com/file-png-image-royalty-free-stock-png-images-for-your-design-file-png-256_256.png"
+            title="Contemplative Reptile"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              Example6.png
+            </Typography>
+            <Typography
+              variant="body2"
+              color="textSecondary"
+              component="p"
+            ></Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button size="small" color="primary">
+            <GetApp style={{ marginRight: "5px" }} />
+            Download
+          </Button>
+          <Button size="small" color="primary">
+            <Visibility style={{ marginRight: "5px" }} />
+            Preview
+          </Button>
+        </CardActions>
+      </Card>
+    </div>
+  );
+};
+
+export default Records;
