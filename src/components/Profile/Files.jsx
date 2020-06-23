5ae8e834ee0db37dec4717d2e12f17c1c07404cf
amirdbt <hassanamir643@yahoo.com>
2020-06-23 23:34:06 +0100

    added a function to check file extention type and display images according to

--- src/components/Profile/Files.jsx
+++ src/components/Profile/Files.jsx
@@ -38,7 +38,24 @@ const Files = ({ match, location }) => {
   console.log(match);
   console.log(location);
   const { files } = location.state.records;
-  console.log(files);
+  function getFile(file) {
+    if (file.includes("pdf")) {
+      return "https://banner2.cleanpng.com/20180420/ypq/kisspng-pdf-computer-icons-theme-clip-art-cool-business-card-background-5ad9c522531736.0976301015242212183404.jpg";
+    } else if (
+      file.includes("doc") ||
+      file.includes("docx") ||
+      file.includes("txt")
+    ) {
+      return "https://banner2.cleanpng.com/20180724/bsf/kisspng-google-docs-computer-icons-microsoft-google-drive-google-icon-5b56c718d11477.1116395315324137208564.jpg";
+    } else if (
+      file.includes("xls") ||
+      file.includes("xlsx") ||
+      file.includes("csv")
+    ) {
+      return "https://banner2.cleanpng.com/20180702/hph/kisspng-computer-icons-google-sheets-5b3a2f1b216e87.3834502715305398031369.jpg";
+    } else return file;
+  }
+
   const classes = useStyles();
   return (
     <div className="content">
@@ -50,18 +67,18 @@ const Files = ({ match, location }) => {
         >
           Records
         </Link>
-        <Link aria-current="page" className={classes.links}>
+        <div aria-current="page" className={classes.links}>
           {location.state.records.record_name.toUpperCase()}
-        </Link>
+        </div>
       </Breadcrumbs>
       <Typography variant="h5">Files</Typography>
       <div className={classes.cards}>
-        {files.map((file) => (
-          <Card className={classes.root}>
+        {files.map((file, index) => (
+          <Card className={classes.root} key={index}>
             <CardActionArea>
               <CardMedia
                 className={classes.media}
-                image={file}
+                image={getFile(file)}
                 title="Patient File"
               />
               <CardContent>
