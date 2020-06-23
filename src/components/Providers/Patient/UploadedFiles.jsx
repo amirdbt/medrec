ab8ae28368502faf519a5d26c63060ae10200d6d
amirdbt <hassanamir643@yahoo.com>
2020-06-23 23:39:57 +0100

    added a function to check file extention type and display images according to

--- src/components/Providers/Patient/UploadedFiles.jsx
+++ src/components/Providers/Patient/UploadedFiles.jsx
@@ -30,6 +30,24 @@ const useStyles = makeStyles((theme) => ({
 const UploadedFiles = ({ files }) => {
   console.log(files);
   const classes = useStyles();
+  function getFile(file) {
+    if (file.includes("pdf")) {
+      return "https://banner2.cleanpng.com/20180420/ypq/kisspng-pdf-computer-icons-theme-clip-art-cool-business-card-background-5ad9c522531736.0976301015242212183404.jpg";
+    } else if (
+      file.includes("doc") ||
+      file.includes("docx") ||
+      file.includes("txt")
+    ) {
+      return "https://banner2.cleanpng.com/20180724/bsf/kisspng-google-docs-computer-icons-microsoft-google-drive-google-icon-5b56c718d11477.1116395315324137208564.jpg";
+    } else if (
+      file.includes("xls") ||
+      file.includes("xlsx") ||
+      file.includes("csv")
+    ) {
+      return "https://banner2.cleanpng.com/20180702/hph/kisspng-computer-icons-google-sheets-5b3a2f1b216e87.3834502715305398031369.jpg";
+    } else return file;
+  }
+
   return (
     <div className={classes.cards}>
       {files.map((file) => (
@@ -37,7 +55,7 @@ const UploadedFiles = ({ files }) => {
           <CardActionArea>
             <CardMedia
               className={classes.media}
-              image={file}
+              image={getFile(file)}
               title="Patient File"
             />
             <CardContent>
